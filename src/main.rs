e207e643cf248a5d0e0d19c45cb98e01b140aa27
khaled kabani <khaledkabbani007@gmail.com>
2026-08-27 16:39:42 +0200

    Add task menu

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
+use std::io;
 
 fn main() {
-
     println!("TASK MANAGER");
 
-    let mut tasks = vec!["study Rust","study git","study Informatik"];
-    tasks.push("study Germman");
-        println!("Tasks: {:?}", tasks);
-    
+    let mut tasks: Vec<String> = vec![];
+
+    loop {
+        let mut choice = String::new();
+
+        println!("1. Add Task");
+        println!("2. View Tasks");
+        println!("3. Quit");
+        println!("Choose an option:");
+        io::stdin().read_line(&mut choice).unwrap();
+
+        match choice.trim() {
+            "1" => println!("Add task"),
+            "2"=> println!("show tasks"),
+            "3" => break,
+            _ => println!("Invalid option"),
+        }
+    }
 }
+    s
